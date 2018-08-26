f0b58ba21fdbd920fc83b53b0e7d857614c0354d
Tyrel Chambers <tychambers3@gmail.com>
2018-08-26 08:00:16 -0400

    styled comments

--- src/pages/User/Dashboard/Account/Account.js
+++ src/pages/User/Dashboard/Account/Account.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import AccountForm from '../../../../components/Forms/AccountForm';
+import './Account.scss';
+import AccountInfo from '../../../../components/AccountInfo/AccountInfo';
 
 export default class Account extends Component {
   render() {
     return (
-      <div>
-        <h1>Account</h1>
-        <AccountForm />
+      <div className="dashboard__panel">
+        <h1 className="dashboard__title">Account</h1>
+        <div className="row account__sub-panel">
+          <main className="account__main">
+            <AccountForm />
+          </main>
+          <aside className="account__aside">
+            <div className="container small">
+              <h3 className="uppercase font small">Your Information</h3>
+              <AccountInfo />
+            </div>
+          </aside>
+        </div>
       </div>
     )
   }
